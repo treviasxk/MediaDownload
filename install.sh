9965a8a271a46e8ef3fcfaf646c52f97ce78460f
Trevias Xk <trevias@live.com>
2021-06-18 14:59:11 -0300

    • Caminho arrumado

--- install.sh
+++ install.sh
@@ -16,9 +16,10 @@
     pkg install ffmpeg -y
     curl -s -L https://yt-dl.org/downloads/latest/youtube-dl -o /data/data/com.termux/files/usr/bin/youtube-dl
     chmod a+rx /data/data/com.termux/files/usr/bin/youtube-dl
-    mkdir /bin
-    curl -s -L https://raw.githubusercontent.com/treviasxk/media-download/main/termux-url-opener -o /bin/termux-url-opener
-    chmod +x /bin/termux-url-opener
+    mkdir $HOME/bin
+    curl -s -L https://raw.githubusercontent.com/treviasxk/media-download/main/termux-url-opener -o $HOME/bin/termux-url-opener
+    chmod +x $HOME/bin/termux-url-opener
     echo -e "\e[30;48;5;82m STATUS \e[40;38;5;82m INSTALADO COM SUCESSO! \e[0m"
-    bash /bin/termux-url-opener
+    rm $HOME/install
+    bash $HOME/bin/termux-url-opener
     exit
